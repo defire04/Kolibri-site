680c2ff477c7c08bc16fd1698fde2fb96e93fe35
Dima Olaba <defire04st@gmail.com>
2021-10-01 17:13:12 +0300

    Added base.html

--- mainapp/models.py
+++ mainapp/models.py
@@ -9,8 +9,6 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
-
-
 # 1 Product
 # 2 Category
 # 3 CartProduct
@@ -19,6 +17,28 @@ User = get_user_model()
 # ************************
 # 6 Customer
 # 7 Specification
+
+
+class LatestProductsManager:
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True)
+        return products
+
+
+class LatestProducts:
+
+    object = LatestProductsManager()
 
 
 class Category(models.Model):
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+def test_view(request):
+    return render(request, 'base.html', {})
